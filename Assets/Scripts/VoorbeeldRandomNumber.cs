028588c8fec23ee706a0e0a0e3837e24b1f6fd5b
xraymonx <raymon_jesse2@hotmail.com>
2016-02-12 10:14:05 +0100

    Random Number generator and a example of the use of it.

---
+++ Assets/Scripts/VoorbeeldRandomNumber.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoorbeeldRandomNumber : MonoBehaviour {
+
+
+	private int randomGained;
+	[SerializeField] private int _MissChance;
+	[SerializeField] private int _BetweenChance;
+	RandomGen randomGen;
+
+
+	void Start () {
+		randomGen = GetComponent<RandomGen>();
+
+	}
+	void Update()
+	{
+		Miss ();
+
+	}
+
+	void Miss()
+	{
+		randomGained = randomGen.RandomNumber (_BetweenChance);
+		if (randomGained > _MissChance) 
+		{
+			Debug.Log ("Hallo ik ben meer dan  " + _MissChance+ " " + randomGained);
+		}
+	}
+}
