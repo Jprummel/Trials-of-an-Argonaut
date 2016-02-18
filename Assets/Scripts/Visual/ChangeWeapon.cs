65cdbf03d87461776541a8e1473d884163900f09
Jeffreeyy <17844@ma-web.nl>
2016-02-18 11:37:21 +0100

    Added Weaponchange

---
+++ Assets/Scripts/Visual/ChangeWeapon.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWeapon : MonoBehaviour 
+{
+    [SerializeField]private GameObject _swordEquipped;
+    [SerializeField]private GameObject _spearEquipped;
+    [SerializeField]private GameObject _shieldEquipped;
+
+    [SerializeField]private GameObject _swordNotEquipped;
+    [SerializeField]private GameObject _spearNotEquipped;
+    [SerializeField]private GameObject _shieldNotEquipped;
+
+    void Start()
+    {
+        EquipSwordAndShield();
+    }
+
+    
+    public void EquipSwordAndShield()
+    {
+        _swordEquipped.SetActive(true);
+        _swordNotEquipped.SetActive(false);
+
+        _spearEquipped.SetActive(false);
+        _spearNotEquipped.SetActive(true);
+
+        _shieldEquipped.SetActive(true);
+        _shieldNotEquipped.SetActive(false);
+    }
+
+    public void EquipSpear()
+    {
+        _swordEquipped.SetActive(false);
+        _swordNotEquipped.SetActive(true);
+
+        _spearEquipped.SetActive(true);
+        _spearNotEquipped.SetActive(false);
+
+        _shieldEquipped.SetActive(false);
+        _shieldNotEquipped.SetActive(true);
+    }
+}
