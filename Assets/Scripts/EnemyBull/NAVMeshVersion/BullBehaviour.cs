d93382deb9c12a106a0b49f56b9469ab5f4ecaa4
xraymonx <raymon_jesse2@hotmail.com>
2016-02-24 15:11:02 +0100

    Bull works with navMesh

---
+++ Assets/Scripts/EnemyBull/NAVMeshVersion/BullBehaviour.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum StateID
+{
+	NullState = 0,
+	IdleState = 1,
+	ChargeState = 2,
+	AttackState = 3,
+	FlameState = 4,
+	RecoverState = 5
+
+}
+public class BullBehaviour : MonoBehaviour {
+
+	private StateMachine _stateMachine;
+
+	private Vector3 _targetPos;
+	public Vector3 targetPos
+	{
+		get{return _targetPos; }
+		set{_targetPos = value; }
+	}
+
+
+	private NavMeshAgent _navComponent;
+
+
+	private float _maxSpeed;
+	public float maxSpeed
+	{
+		get{return _maxSpeed;}
+		set{ _maxSpeed = value;}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		_navComponent = GetComponent<NavMeshAgent> ();
+		_stateMachine = GetComponent<StateMachine> ();
+
+		MakeStates ();
+
+		_stateMachine.SetState (StateID.IdleState);
+	}
+
+	void MakeStates()
+	{
+		_stateMachine.AddState (StateID.IdleState, GetComponent<StateIdle> () );
+		_stateMachine.AddState (StateID.ChargeState, GetComponent<StateCharge> ());
+		_stateMachine.AddState (StateID.RecoverState, GetComponent<StateRecover> ());
+	}
+	// Update is called once per frame
+	void Update () {
+
+		if(_targetPos != null)
+		{
+			_navComponent.SetDestination(_targetPos);
+			//DistanceTo ();
+
+		}
+	}
+
+	public void setSpeed(float newSpeed)
+	{
+		_navComponent.speed = newSpeed;
+
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/EnemyBull/NAVMeshVersion/StateRecover.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateRecover : StateParent {
+
+	[SerializeField] private float _maxSpeed = 20f;
+	[SerializeField] private float _waitForRecover = 1f;
+
+	private Vector3 _startPos;
+	private Vector3 _currentPosition;
+
+	private Vector3 _mainTarget;
+	private Vector3 _newTarget;
+
+
+	BullBehaviour bullbehaviour;
+	StateIdle stateIdle; 
+
+	public override void Enter ()
+	{
+		_startPos = transform.position;
+
+		bullbehaviour = GetComponent<BullBehaviour> ();
+		stateIdle = GetComponent<StateIdle> ();
+
+		bullbehaviour.setSpeed (_maxSpeed);
+
+		_mainTarget = stateIdle.targetPlayer.transform.position;
+		_newTarget = _mainTarget + _startPos;
+
+		bullbehaviour.targetPos = _newTarget;
+
+		StartCoroutine (Waiting (_waitForRecover));
+	}
+
+	public override void Leave ()
+	{
+
+	} 
+
+	public override void Act ()
+	{
+		
+
+	}
+
+	public override void Reason ()
+	{
+
+	}
+
+	IEnumerator Waiting(float waitTime)
+	{
+		yield return new WaitForSeconds (waitTime);
+		GetComponent<StateMachine> ().SetState (StateID.IdleState);
+
+	}
+}
