e166ed2cb1732e59a5b9b5838e448f1ffcfbca62
Jeffreeyy <17844@ma-web.nl>
2016-02-24 14:04:07 +0100

    Fixed pickup

--- Assets/Scripts/Visual/HealthPickup.cs
+++ Assets/Scripts/Visual/HealthPickup.cs
@@ -6,32 +6,36 @@ public class HealthPickup : MonoBehaviour
     private Health _health;
     [SerializeField]private float _healthToAdd;
     [SerializeField]private float _timeToRespawn;
+    [SerializeField]private GameObject _pickUpObject;
+    private BoxCollider _pickUpCollider;
 
     void Start()
     {
         _health = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        _pickUpCollider = GetComponent<BoxCollider>();
     }
 
     public void AddHealth(float value)
     {
+        StartCoroutine(PickupCooldown());
         _health.health += _healthToAdd * value;
         if(_health.health > _health.maxHealth)
         {
             _health.health = _health.maxHealth;
         }
-        StartCoroutine(PickupCooldown());
     }
 
     IEnumerator PickupCooldown()
     {
-        gameObject.SetActive(false);
+        _pickUpObject.SetActive(false);
+        _pickUpCollider.enabled = false;
         yield return new WaitForSeconds(_timeToRespawn);
         RespawnPickup();
-
     }
 
     void RespawnPickup()
     {
-        gameObject.SetActive(true);
+        _pickUpObject.SetActive(true);
+        _pickUpCollider.enabled = true;
     }
 }
