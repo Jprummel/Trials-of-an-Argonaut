094ec2c0229b7b7f605675264be8db914b98549a
Jeffreeyy <17844@ma-web.nl>
2016-03-14 09:14:28 +0100

    Blood Particles

---
+++ Assets/Scripts/Visual/ParticleSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSpawner : MonoBehaviour {
+
+    [SerializeField]private GameObject[] _bloodParticles;
+    [SerializeField]private Transform _particleSpawnPoint;
+
+    public void SpawnParticles(int particleIndex)
+    {
+        GameObject bloodParticles = (GameObject)Instantiate(_bloodParticles[particleIndex], _particleSpawnPoint.position, _bloodParticles[particleIndex].transform.rotation) as GameObject;
+        bloodParticles.transform.parent = _particleSpawnPoint.transform;
+    }
+}
