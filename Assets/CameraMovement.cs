1bc5351231df3fd134ddab6c7c871eeee5ba95e2
Jprummel <jordiprummel@live.nl>
2016-03-31 11:32:36 +0200

    Main scene update

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -3,16 +3,13 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-    [SerializeField]
-    private Transform _playerPos;
-    [SerializeField]
-    private float _minY;
-    [SerializeField]
-    private float _maxY;
-    private float _cameraY;
-    private float _rotationY;
-    private float _rotationX;
-    public Vector3 cameraForward;
+    [SerializeField]private Transform   _playerPos;
+    [SerializeField]private float       _minY;
+    [SerializeField]private float       _maxY;
+                    private float       _cameraY;
+                    private float       _rotationY;
+                    private float       _rotationX;
+                    public Vector3      cameraForward;
 
     void Update()
     {
@@ -36,16 +33,13 @@ public class CameraMovement : MonoBehaviour {
     {
         //transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * value);
         _rotationX += Time.deltaTime * rotationSpeed * value;
-
     }
 
     public void RotateY(float camSensitivity, float value)
     {
-
         //transform.eulerAngles   = new Vector3(_rotationY, transform.localEulerAngles.y);
-        _rotationY             += Time.deltaTime * camSensitivity * value;
-        _rotationY = Mathf.Clamp(_rotationY, _minY, _maxY);
-
+        _rotationY += Time.deltaTime * camSensitivity * value;
+        _rotationY  = Mathf.Clamp(_rotationY, _minY, _maxY);
     }
 
     private void UpdateRotation()
