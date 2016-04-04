5ded578b5f116b33c11b286b1f387227b5da2977
xraymonx <raymon_jesse2@hotmail.com>
2016-04-04 14:31:41 +0200

    particles on pillar collision

--- Assets/Scripts/terrain/TowerDamage.cs
+++ Assets/Scripts/terrain/TowerDamage.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class TowerDamage : MonoBehaviour {
 
 	       Animator _animator;
+	[SerializeField] private GameObject _particles;
+	private AudioSource _audioCollapse;
     public bool     doDamage = true;
 
 	void Start () {
 		_animator = GetComponentInParent<Animator> ();
+		_audioCollapse = GetComponent<AudioSource> ();
 	}
 
 	public void CheckForPlay()
 	{
 		if (doDamage) 
         {
+			
             _animator.SetBool ("DidHit", true);
+			_particles.SetActive (true);
+			_audioCollapse.Play ();
 			doDamage = false;
 		}
 	}
