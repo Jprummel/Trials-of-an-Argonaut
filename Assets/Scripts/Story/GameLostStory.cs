834ae00380efd15f2eaf5c89cf4c26c91d7d370d
Jeffreeyy <17844@ma-web.nl>
2016-04-08 16:47:10 +0200

    fix

--- Assets/Scripts/Story/GameLostStory.cs
+++ Assets/Scripts/Story/GameLostStory.cs
@@ -14,11 +14,12 @@ public class GameLostStory : MonoBehaviour {
     IEnumerator GameLost()
     {
         _storyText.text = "The gods did not favor Jason on that day";
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(3f);
         _storyText.text = "Pelias remained the king and his people suffered under his reign";
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(5f);
         _storyText.text = "Maybe next time the gods will send a real hero.";
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(4f);
+        _storyText.text = "";
         _buttons.SetActive(true);
     }
 
--- Assets/Scripts/Story/GameWonStory.cs
+++ Assets/Scripts/Story/GameWonStory.cs
@@ -14,17 +14,18 @@ public class GameWonStory : MonoBehaviour {
     IEnumerator EndGameStory()
     {
         _storyText.text = "And so Jason finished of the Khalkotauroi once and for all.";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
         _storyText.text = "After the fight Jason challenged King Pelias to a duel for the throne";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
         _storyText.text = "King Pelias did not feel much for the idea of a battle with Jason.";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(4f);
         _storyText.text = "But after a hefty chant from the audience 'Finish him! show him who he is dealing with!' Pelias had no choice but to face Jason. ";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(6f);
         _storyText.text = "The fight wasn't much of a spectacle as Jason almost instantly forced Pelias down to his knees and removed his head with a single swift cut";
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(6f);
         _storyText.text = "Iolcos now has a new king and his name is Jason";
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(4f);
+        _storyText.text = "";
         _buttons.SetActive(true);
     }
 }
