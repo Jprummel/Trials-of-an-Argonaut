d2b5c05aa64fa58f496dfa58a2c73dbf34558442
Jprummel <jordiprummel@live.nl>
2016-04-08 14:40:04 +0200

    Added outcomehandler again

---
+++ Assets/Scripts/Story/OutcomeHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OutcomeHandler : MonoBehaviour {
+
+    [SerializeField]private GameObject _bull;
+    [SerializeField]private GameObject _player;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_bull == null)
+        {
+            SceneManager.LoadScene(3);
+        }
+        if(_player == null)
+        {
+            SceneManager.LoadScene(4);
+        }
+	}
+
+
+}
